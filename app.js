e9a5e9f464de3974ba417709496807466875084a
Xalilov Baxrullo <109218118+baxaaka@users.noreply.github.com>
2024-01-16 22:40:58 +0500

    1

--- app.js
+++ app.js
@@ -1,23 +1,42 @@
-"use strict";
-
 const button = document.querySelector("button");
-let inputField = document.querySelector("input");
-let input = inputField.value.trim();
+let count = document.querySelector(".count");
+let reset = document.querySelector(".reset");
+
+let countt = parseInt(localStorage.getItem("count")) || 0;
+count.textContent = countt;
 
 button.addEventListener("click", (e) => {
-  input = (parseInt(input) + 1).toString();
-  localStorage.setItem("inputCount", input);
-
-  const count = localStorage.getItem("inputCount");
-  console.log("Count:", count);
-
- 
-  inputField.value = count;
-
-  play(); 
+  countFunction();
+  play();
 });
+
+reset.addEventListener("click", () => {
+  resetCount()
+})
+
+
+function countFunction() {
+  countt++;
+  localStorage.setItem("count", countt);
+
+  count.textContent = countt;
+}
+
+
+
+function resetCount() {
+  let alertt = confirm("boshidan boshlash")
+
+  if (alertt) {
+    window.location.reload()
+    localStorage.setItem("count", 0)
+  }
+
+}
 
 function play() {
   var audio = new Audio('./audio/button.mp3');
   audio.play();
 }
+
+
